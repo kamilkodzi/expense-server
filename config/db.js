5d9df8040f66f77b43118a376643f626caffc06f
kamilkodzi <bizkamiladamski@gmail.com>
2022-06-09 21:11:39 +0200

    mongo + test routes

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/test");
+}
+
+module.exports = main();
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-
+const routes = require("./routes/routes.js");
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Gaming-server!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.use("/api", routes);
+
+app.listen(3000, () => {
+  console.log(`Server starts on port 3000`);
 });
