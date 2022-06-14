4fee90c9b295f681dcb66fbfb15db07bf703b18c
kamilkodzi <bizkamiladamski@gmail.com>
2022-06-14 21:17:00 +0200

    regrister route added - in pour poor condition

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
+  name: String,
   username: String,
   password: String,
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
 const router = Router();
 const passport = require("passport");
+const User = require("../models/User");
 
 router.post("/login", passport.authenticate("local"), (req, res) => {
   res.send(200);
+});
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const newUser = await User.create({ username: username, password: password });
+  console.log(newUser);
+  res.send(newUser);
 });
 
 module.exports = router;
